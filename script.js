aec4f9fc503f9826c24b8791f9c8a08330638185
Ethan Leitner <57595571+litehed@users.noreply.github.com>
2023-01-13 10:58:38 -0500

    Fixed Laser and Stopped spawning stars in main doc

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var angle = 0;
 
 
 document.onmousemove = function(event) {
-    angle = Math.atan2(event.pageX - shipCoords.x, - (event.pageY - shipCoords.y) )*(180 / Math.PI);  
+    angle = Math.atan2(event.x - shipCoords.x, - (event.y - shipCoords.y) )*(180 / Math.PI);
     ship.style.transform = "rotate(" + angle + "deg)";
 }
 
@@ -16,28 +16,32 @@ document.onclick = function(event) {
     let laserAngle = angle;
     let laser = document.createElement("div");
     laser.classList.add("laser");
-    laser.style.top = ship.offsetTop + ship.clientHeight/2 + "px";
-    laser.style.left = ship.offsetLeft + ship.clientWidth/2 + "px";
+    // document.write("shipCoords.x: " + shipCoords.x + " clientWidth/2: " + ship.clientWidth/2);
+    laser.style.top = shipCoords.y + (ship.clientHeight/2 * Math.sin((laserAngle-90) * (Math.PI/180))) + "px";
+    laser.style.left = shipCoords.x + (ship.clientWidth/2 * Math.cos((laserAngle-90) * (Math.PI/180))) + "px";
     laser.style.transform = "rotate(" + laserAngle + "deg)";
     document.body.appendChild(laser);
     function animateLaser() {
-    laser.style.top = laser.offsetTop - (Math.sin(laserAngle * (Math.PI / 180)) * 10) + "px";
-    laser.style.left = laser.offsetLeft + (Math.cos(laserAngle * (Math.PI / 180)) * 10) + "px";
-    if (laser.offsetLeft > window.innerWidth || laser.offsetTop < 2 || laser.offsetLeft < 2 || laser.offsetTop > window.innerHeight) {
-        laser.remove();
-    } else {
-        requestAnimationFrame(animateLaser);
-    }
+        laser.style.top = laser.offsetTop + (Math.sin((laserAngle-90) * (Math.PI / 180)) * 10) + "px";
+        laser.style.left = laser.offsetLeft + (Math.cos((laserAngle-90) * (Math.PI / 180)) * 10) + "px";
+        if (laser.offsetLeft > window.innerWidth || laser.offsetTop < 2 || laser.offsetLeft < 2 || laser.offsetTop > window.innerHeight) {
+            laser.remove();
+        } else {
+            requestAnimationFrame(animateLaser);
+        }
     }
     animateLaser();
 }
+
+const starContainer = document.getElementById('stars');
+
   
 function createStar() {
     var star = document.createElement("div");
     star.classList.add("star");
     star.style.top = Math.floor(Math.random() * (100 - 0 + 1) + 0) + "%";
     star.style.left = Math.floor(Math.random() * (100 - 0 + 1) + 0) + "%";
-    document.body.appendChild(star);
+    starContainer.appendChild(star);
     
     function animateStar() {
         star.style.left = star.offsetLeft - 1 + "px";
@@ -51,4 +55,4 @@ function createStar() {
     animateStar();
 }
 
-setInterval(createStar, 50);
+setInterval(createStar, 100);
